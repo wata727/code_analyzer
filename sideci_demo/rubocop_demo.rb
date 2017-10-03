f733767007983d6f6710819c12ce7ab7c32039d7
SideCI <support@sideci.com>
2017-10-03 14:08:52 +0900

    SideCI Demo

---
+++ sideci_demo/rubocop_demo.rb
@@ -0,0 +1,19 @@
+class Cat
+  def meow
+    puts "meow!"
+  end
+end
+
+# Lint/DuplicateKey
+# Lint/EnsureReturn
+# Lint/IneffectiveAccessModifier
+# Lint/InheritException
+# Lint/SafeNavigationChain
+# Lint/ShadowedException
+# Performance/CompareWithBlock
+# Performance/DoubleStartEndWith
+# Performance/FlatMap
+# Performance/HashEachMethods
+# Performance/ReverseEach
+# Performance/Sample
+# Performance/StartWith
